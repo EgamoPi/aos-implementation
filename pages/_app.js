be12e1544284856fedaf378f356a54a051e5e830
j.sekretev@icloud.com <j.sekretev@icloud.com>
2020-10-28 20:12:58 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import AOS from "aos";
+import "../styles/globals.css";
+
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		AOS.init({
+			delay: 2000,
+			duration: 800,
+		});
+	});
+
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
