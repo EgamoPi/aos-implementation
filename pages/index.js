ee84cd3e80f70a2c9d7193999603d5626b1232d0
j.sekretev@icloud.com <j.sekretev@icloud.com>
2020-10-30 20:53:52 +0000

    added notes for Jacinto to help me

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { createElement as $ } from "react";
+import styles from "../styles/Home.module.css";
 import sectionOne from "../components/sectionOne";
 import sectionTwo from "../components/sectionTwo";
 import sectionThree from "../components/sectionThree";
 import Menu from "../components/Menu";
 
+//For jacinto
+// Of course toggleNav and navAnimation function will not work since their variables are not declared
+// Because cant use getElementById in react, because of how it renders
+//From googling I should be using refs, but refs only work on class component not functional component
+//Can you help me ?
+// Also I had to change to camelCase just to follow "the way" of noting things.
+
 //Function Declaration
 function navAnimation(direction1, direction2) {
 	navItems.forEach((nav, index) => {
 		nav.classList.replace(
-			`slide-${direction1}-${index + 1}`,
-			`slide-${direction2}-${index + 1}`
+			`slide${direction1}${index + 1}`,
+			`slide${direction2}${index + 1}`
 		);
 	});
 }
@@ -25,12 +31,12 @@ function toggleNav() {
 		//Animate In - Overlay
 		overlay.classList.replace("overlaySlideLeft", "overlaySlideRight");
 		//Animate In - Nav Items
-		navAnimation("out", "in");
+		navAnimation("Out", "In");
 	} else {
 		//Animate Out - Overlay
 		overlay.classList.replace("overlaySlideRight", "overlaySlideLeft");
 		//Animate Out - Nav Items
-		navAnimation("in", "out");
+		navAnimation("In", "Out");
 	}
 }
 
